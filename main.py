1ec3fbc14cad7b43ccf01d8b2429c4df475d33cd
Tomasz-Gziut <tomasz.g.direct@gmail.com>
2025-11-04 16:12:04 +0100

    create(middleware): make it local/production dependent

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.users import router as users_router
 from routers.notes import router as notes_router
 from routers.login import router as login_router
+from config import ALLOWED_ORIGINS
 
 app = FastAPI(title="Cheatsheets API", version="1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(login_router)
 app.include_router(users_router)
